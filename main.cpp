77eed1bcc37753ea13a050b6aafda53caab864ee
Lucas Dietrich <ld.adecy@gmail.com>
2021-05-30 20:52:43 +0200

    tested newly implemented can_device class

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-#include <caniot/caniot.h>
+#include <mcp_can.h>
+#include <caniot/device.h>
+#include <caniot/hw_init.h>
 #include <uart.h>
 
-can_device can;
+#define SPI_CS_PIN  10
+#define CAN_INT_PIN 2
+
+mcp2515_can can(SPI_CS_PIN);
+can_device dev(&can, CAN_INT_PIN, CAN_500KBPS, MCP_8MHz);
 
 int main()
 {
+    hw_init();
+
     usart_init();
 
-    usart_u8((uint8_t) can.m_id);
+    dev.print_identification();
+
+    dev.initialize();
 
     while (1)
     {
-
+        if (dev.flag_can)
+        {
+            dev.process_message();
+        }
     }
 }
