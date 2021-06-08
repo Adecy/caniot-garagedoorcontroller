4a7ba3e146b18c05ab168dc6e6ffa1018f706686
Lucas Dietrich <ld.adecy@gmail.com>
2021-06-09 00:05:46 +0200

    added uptime and abstime

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@
 #include <caniot/hw_init.h>
 #include <uart.h>
 
+#include <errno.h>
+
 #define SPI_CS_PIN  10
 #define CAN_INT_PIN 2
 
 mcp2515_can can(SPI_CS_PIN);
 can_device dev(&can, CAN_INT_PIN, CAN_500KBPS, MCP_8MHz);
+
+uint32_t time_last = 0;
+uint32_t time;
 
 int main()
 {
@@ -23,11 +28,21 @@ int main()
 
     dev.initialize();
 
+    usart_u16(sizeof(struct can_device::attributes));
+    
     while (1)
     {
-        if (dev.flag_can)
+        dev.process();
+
+        // print updatime
+        time = dev.uptime();
+        if (time - time_last >= 10)
         {
-            dev.process();
+            time_last = time;
+            print_time_sec(time);
+
+            usart_hex16(dev.abstime() >> 16);
+            usart_hex16(dev.abstime());
         }
     }
 }
